7ca7173a1a2b34fed0c547d198d6a04d9a3e5097
shaik-adnan1 <yousufwrk0@gmail.com>
2024-09-20 12:53:05 +0530

    morgan and cofigs setup

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-const express = require('express');
-
+const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
+// json parsing middleware
 app.use(express.json());
+
+// Morgan for API logs(response time, endpoints hit and response codes)
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("combined"));
+}
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,14 +1,17 @@
-const app = require('./app');
-const dotenv = require('dotenv');
+const app = require("./app");
+const dotenv = require("dotenv");
 
-
-
+// .env config
 dotenv.config({
-    path: `${__dirname}/config.env`
+  path: `${__dirname}/config.env`,
 });
+
+// Need to connect to a DB
+// MONGODB
 
 const port = process.env.PORT;
 
+// Server running here
 app.listen(port, () => {
-    console.log(`app running successfully on port ${port}`);
-})
+  console.log(`app running successfully on port ${port}`);
+});
