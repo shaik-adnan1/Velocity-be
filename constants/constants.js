5f7ffaad39b5d5be2f99851a816da998a0095128
Sk. Yousuf Adnan_S <121503022+shaik-adnan1@users.noreply.github.com>
2024-10-01 12:49:44 +0530

    Revert "Revert "updated create user it take hased password""

--- constants/constants.js
+++ constants/constants.js
@@ -1,9 +1,18 @@
 const constants = {
   SUCCESS_CODE: "0000",
+  SUCCESS_STATUS: "Success",
+  FAILURE_STATUS: "Failure",
+
+  
+  VALIDATION_ERROR: "4000",
+  DUPLICATE_KEY_ERROR: "4001",
+  SOMETHING_WENT_WRONG: "5000",
+
   FAILURE_CODE: "0001",
   SUCCESS_MESSAGE: "Success",
   VERIFICATION_FAILURE: "VERIFICATION_FAILED",
   INPUT_VALIDATION_FAIL: "Input Validation Failed",
+  SUCCESS_MESSAGE: "API call Successful",
 };
 // dummy comment
 module.exports = constants;
