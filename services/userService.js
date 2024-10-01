a56bafe39c0872b54f88b7e8f11e858535000166
Sk. Yousuf Adnan_S <121503022+shaik-adnan1@users.noreply.github.com>
2024-10-01 12:40:08 +0530

    Revert "updated create user it take hased password"

--- services/userService.js
+++ services/userService.js
@@ -1,55 +1,63 @@
-const bcrypt = require("bcryptjs");
-
 const User = require("../models/userModel");
-const constants = require("../constants/constants");
 const { createUserValidation } = require("../services/validationService");
-const { mongooseErrorHandler } = require("../utils/mongooseErrorHandler");
-const { successResponse, customResponse } = require("../utils/customResponse");
 
 exports.createUserService = async (userDetails) => {
   // console.log("reqbody", req);
-  // const { username, mobile, fistName, lastName } = userDetails;
+  const { username, mobile, fistName, lastName } = userDetails;
 
-  const SALT_ROUNDS = 10;
-
-  try {
-    const validationErrors = createUserValidation(userDetails);
-    if (validationErrors.length > 0) {
-      return customResponse(
-        constants.VALIDATION_ERROR,
-        constants.FAILURE_STATUS,
-        constants.INPUT_VALIDATION_FAIL,
-        validationErrors
-      );
-    }
-
-    const hashedPassword = await bcrypt.hash(userDetails.password, SALT_ROUNDS);
-    userDetails.password = hashedPassword;
-
-    // not storing confirm password into DB
-    delete userDetails.confirmPassword;
-
-    const newUser = await User.create(userDetails);
-
-    console.log(`Successfully created user ${newUser.username}`);
-
-    return successResponse({
-      newUser,
-    });
-  } catch (error) {
-    const normalizedError = mongooseErrorHandler(error);
-    return customResponse(
-      constants.VALIDATION_ERROR,
-      constants.FAILURE_STATUS,
-      normalizedError.message,
-      normalizedError.details
-    );
-    
-    // {
-    //   code: constants.VALIDATION_ERROR,
-    //   status: "Failure",
-    //   message: normalizedError.message,
-    //   details: normalizedError.details,
-    // };
+  if (!username || username === "") {
+    throw {
+      status: "Failure",
+      message: "Please enter a correct username",
+      details: {
+        message: "Incorrect username",
+      },
+    };
   }
-}
+
+  if (!mobile || mobile === "") {
+    throw {
+      status: "Failure",
+      message: "Please enter a correct mobile",
+      details: {
+        message: "Incorrect mobile",
+      },
+    };
+  }
+
+  if (!fistName || fistName === "") {
+    throw {
+      status: "Failure",
+      message: "Please enter a correct fistName",
+      details: {
+        message: "Incorrect fistName",
+      },
+    };
+  }
+
+  if (!lastName || lastName === "") {
+    throw {
+      status: "Failure",
+      message: "Please enter a correct lastName",
+      details: {
+        message: "Incorrect lastName",
+      },
+    };
+  }
+
+  const createValidation = createUserValidation(userDetails);
+  console.log(createValidation);
+
+  const newUser = await User.create(userDetails);
+
+  return {
+    status: "Success",
+    message: "Received user details",
+    details: {
+      username,
+      mobile,
+      fistName,
+      lastName,
+    },
+  };
+};
--- utils/customResponse.js
+++ utils/customResponse.js
@@ -3,16 +3,14 @@ const constants = require("../constants/constants");
 const successResponse = (details) => {
   return {
     code: constants.SUCCESS_CODE,
-    status: constants.SUCCESS_STATUS,
     message: constants.SUCCESS_MESSAGE,
     details,
   };
 };
 
-const customResponse = (code, status, message, details) => {
+const customResponse = (code, message, details) => {
   return {
     code,
-    status,
     message,
     details,
   };
