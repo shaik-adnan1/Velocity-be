17e8cd07705adf4c8f58fc94b3fc2f2fb9137419
Yaswanth1320 <110328658+Yaswanth1320@users.noreply.github.com>
2024-10-19 15:00:40 +0530

    Feature/otpverification (#9)
    * twilio client fix
    
    * merge fixes
    
    * resolve requested comments
    
    * resolve requested comments

--- controller/otpController.js
+++ controller/otpController.js
@@ -6,11 +6,19 @@ const {
 } = require("../utils/customResponse.js");
 const constants = require("../constants/constants.js");
 
+
 exports.sendOtp = async (req, res) => {
   const { number } = req.body;
+  if (!number || !/^\+\d{1,15}$/.test(number)) {
+    return res
+      .status(400)
+      .json(
+        customResponse(constants.FAILURE_CODE, "Invalid phone number format.")
+      );
+  }
   try {
     client.verify.v2
-      .services(process.env.TWILIO_ACCOUNT_SID)
+      .services(process.env.TWILIO_AUTH_SERVICE_TOKEN)
       .verifications.create({
         to: number,
         channel: "sms",
@@ -35,10 +43,9 @@ exports.sendOtp = async (req, res) => {
 
 exports.verifyOtp = async (req, res) => {
   const { code } = req.body;
-  console.log(code);
   try {
     const verificationCheck = await client.verify.v2
-      .services(process.env.TWILIO_ACCOUNT_SID)
+      .services(process.env.TWILIO_AUTH_SERVICE_TOKEN)
       .verificationChecks.create({
         code,
         to: "+917416279050",
