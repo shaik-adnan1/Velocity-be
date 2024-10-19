0a38e0cf60c5aaee40dd64c5475a04e9773ed612
Yaswanth1320 <yashwanth.paravathala@gmail.com>
2024-10-19 14:58:53 +0530

    resolve requested comments

--- controller/otpController.js
+++ controller/otpController.js
@@ -6,49 +6,6 @@ const {
 } = require("../utils/customResponse.js");
 const constants = require("../constants/constants.js");
 
-//   const { number } = req.body;
-//   try {
-//     client.verify.v2
-//       .services("VA9391ad1effed1b94b84b0ba299a6faf4")
-//       .verifications.create({ to: number, channel: "sms" })
-//       .then((verification) =>
-//         res.status(200).json(
-//           successResponse({
-//             status: verification.status,
-//             message: "Otp sent successfully",
-//           })
-//         )
-//       );
-//   } catch (error) {
-//     res
-//       .status(404)
-//       .json(
-//         customResponse(constants.SUCCESS_CODE, "Failed to send OTP", error)
-//       );
-//   }
-// };
-
-// exports.testVerify = async (req, res) => {
-//   const { code } = req.body;
-//   try {
-//     const verificationCheck = await client.verify.v2
-//       .services("VA9391ad1effed1b94b84b0ba299a6faf4")
-//       .verificationChecks.create({ to: "+917416279050", code });
-
-//     return res.status(200).json(
-//       successResponse({
-//         status: verificationCheck.status,
-//         message: "Verified Successfully",
-//       })
-//     );
-//   } catch (error) {
-//     res
-//       .status(404)
-//       .json(
-//         customResponse(constants.FAILURE_CODE, "OTP verification Failed", error)
-//       );
-//   }
-// };
 
 exports.sendOtp = async (req, res) => {
   const { number } = req.body;
